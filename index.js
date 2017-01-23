e75acb67e9a79312880a0927f63905924a3c6386
Mark Perkins <mark@allmarkedup.com>
2017-01-23 14:21:52 +0000

    Add xo linting, fixes

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const path = require('path');
-const Promise = require('bluebird');
-const stat = Promise.promisify(require('fs').stat);
-const readdir = Promise.promisify(require('fs').readdir);
+const fs = require('fs');
+const Bluebird = require('bluebird');
+
+const stat = Bluebird.promisify(fs.stat);
+const readdir = Bluebird.promisify(fs.readdir);
 
 module.exports = function read(dir, filter) {
-    filter = filter || (() => true);
-    return readdir(dir)
+  filter = filter || (() => true);
+  return readdir(dir)
     .map(name => {
-        const filePath = path.join(dir, name);
-        return stat(filePath).then(stats => {
-            if (stats.isDirectory()) {
-                return read(filePath, filter).then(paths => {
-                    if (filter(filePath, stats)) {
-                        return [filePath, ...paths];
-                    }
-                    return paths;
-                });
-            }
+      const filePath = path.join(dir, name);
+      return stat(filePath).then(stats => {
+        if (stats.isDirectory()) {
+          return read(filePath, filter).then(paths => {
             if (filter(filePath, stats)) {
-                return filePath;
+              return [filePath, ...paths];
             }
-        });
-    })
-    .reduce((a, b) => b ? a.concat(b) : a, []); // remove empty items and flatten nested arrays
+            return paths;
+          });
+        }
+        if (filter(filePath, stats)) {
+          return filePath;
+        }
+      });
+    }).reduce((a, b) => b ? a.concat(b) : a, []); // remove empty items and flatten nested arrays
 };
